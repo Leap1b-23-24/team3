9c411320041dffb07e605e7414575f93291e7c9d
Altangerel Gantulga <23LP1264@G301-CC0202.local>
2024-03-11 13:09:58 +0800

    ff

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import Carousel from "@/components/Carousel1";
 export default function Home() {
   return (
     <>
-      <Header></Header>
+      <Header />
       <div className="max-w-[1240px] m-auto py-[24px]">
         <h2 className="font-bold text-[28px] py-[16px]">Санал болгож буй</h2>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 place-content-center place-items-center gap-5">
@@ -30,7 +30,7 @@ export default function Home() {
           <SingleCardOfHomePage />
         </div>
       </div>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 }
