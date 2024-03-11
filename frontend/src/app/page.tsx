0ad23a9cd2cbabfb15c380c6f8a23c75c5d0e67e
Бат-Эрдэнэ Хадбаатар <23LP6564@G301-CC0210.local>
2024-03-11 10:07:23 +0800

    done

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,8 @@ import { SingleCardOfHomePage } from "@/components";
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex">
+    <div className="max-w-[1240px] m-auto ">
+      <div className=" grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-5 place-content-center place-items-center gap-5  ">
         {new Array(4).fill(<SingleCardOfHomePage />)}
       </div>
     </div>
