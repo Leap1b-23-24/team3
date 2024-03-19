b26e9a56fd21c7aee2b386987e58ad857d94ac94
undraa btrdni <undraabtrdni@Buffys-MacBook-Pro.local>
2024-03-19 14:53:46 +0800

    done

--- frontend/src/components/AdminTools/dashboard/Dashboard.tsx
+++ frontend/src/components/AdminTools/dashboard/Dashboard.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { Stack } from "@mui/material";
 import DashboardTable from "./DashBoardTable";
-import { DashboardHead } from "@/components/DashboardHead";
 
 export default function Dashboard() {
   return (
     <Stack>
-      <DashboardHead />
       <DashboardTable />
     </Stack>
   );
