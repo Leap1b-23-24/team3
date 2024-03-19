a0c23cb565b7be131b9e883540b72d7212076576
undraa btrdni <undraabtrdni@Buffys-MacBook-Pro.local>
2024-03-19 11:51:28 +0800

    done

--- frontend/src/components/AdminTools/income/Income.tsx
+++ frontend/src/components/AdminTools/income/Income.tsx
@@ -1,9 +1,13 @@
 "use client";
 import { Stack } from "@mui/material";
 import IncomeTable from "./IncomeTable";
+import IncomeHeader from "@/components/IncomHead";
 
 export default function Income() {
-  return <Stack>
-       <IncomeTable/>
-  </Stack>;
+  return (
+    <Stack>
+      <IncomeHeader />
+      <IncomeTable />
+    </Stack>
+  );
 }
