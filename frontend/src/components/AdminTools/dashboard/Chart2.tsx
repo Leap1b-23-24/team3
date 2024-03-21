aa029a13087bff512b1b2e2af3c29829b459d1b9
Altangerel Gantulga <23LP1264@G301-CC0202.local>
2024-03-21 13:35:26 +0800

    unit

--- frontend/src/components/AdminTools/dashboard/Chart2.tsx
+++ frontend/src/components/AdminTools/dashboard/Chart2.tsx
@@ -3,22 +3,29 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 export default function Chart2() {
   return (
     <Stack>
-      <Card>
-        <Stack direction="row">
-          <Typography>Идэвхтэй бүс нутаг</Typography>
-          <ArrowForwardIosIcon />
+      <Card className="max-w-[565px] p-4">
+        <Stack
+          direction="row"
+          className="w-full justify-between items-center mb-5"
+        >
+          <Typography fontSize="18px" fontWeight="600">
+            Идэвхтэй бүс нутаг
+          </Typography>
+          <ArrowForwardIosIcon sx={{ height: "17px" }} />
         </Stack>
-        <Stack>
-          <Typography></Typography>
-        </Stack>
-        <Stack>
-          <Typography></Typography>
-        </Stack>
-        <Stack>
-          <Typography></Typography>
-        </Stack>
-        <Stack>
-          <Typography></Typography>
+        <Stack className="h-[216px] justify-between">
+          <Stack>
+            <Typography>Улаанбаатар</Typography>
+          </Stack>
+          <Stack>
+            <Typography>Дархан </Typography>
+          </Stack>
+          <Stack>
+            <Typography>Эрдэнэт </Typography>
+          </Stack>
+          <Stack>
+            <Typography>Бусад</Typography>
+          </Stack>
         </Stack>
       </Card>
     </Stack>
