938bd369aabd9000461185595e42a0268ef23901
Бат-Эрдэнэ Хадбаатар <23LP6564@G301-CC0210.local>
2024-03-22 13:30:43 +0800

    gg

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 import { Footer } from "@/components";
-import { Stack, Typography } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 
 export default function Home() {
   return (
     <>
       <Stack className="bg-[#F2F0FF] h-[764px] w-full">
-        <Stack className="max-w-[1240px] m-auto">
+        <Stack
+          flexDirection="row"
+          className="max-w-[1240px] m-auto items-center"
+        >
+          <>
+            <p className="text-base text-[#FB2E86]">
+              Тав тухтай орчинг таны амьдралд
+            </p>
+            <p className="text-[53px] font-extrabold">
+              2024 оны хамгийн шинэ загвар
+            </p>
+            <p className="text-[#8A8FB9] text-base">
+              Швед улсын хамгийн шилдэг брэндийг албан ёсны эрхтэйгээр оруулж
+              ирж байна
+            </p>
+            <Button className="bg-[#FB2E86] w-[163px] h-[50px] hover:bg-[#e0196f] text-white text-base">
+              Дэлгэрэнгүй
+            </Button>
+          </>
           <Stack className=" w-[705px]  h-[689px] max-h-[690px] relative">
             <Stack className="w-[650px] h-[650px] bg-[#ECD2FA59] rounded-full absolute  top-0 right-0" />
             <Stack className="w-[650px] h-[650px] bg-[#ECD2FA59] rounded-full absolute bottom-0 left-0" />
