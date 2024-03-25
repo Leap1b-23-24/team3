eaa9e71723bf8f136dfa4b8f94d060f88c6b4ca5
Altangerel Gantulga <23LP1264@G301-CC0202.local>
2024-03-25 10:36:12 +0800

    shop

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,67 +1,9 @@
 import { Footer, Header, SingleCardOfHomePage } from "@/components";
-import WindowIcon from "@mui/icons-material/Window";
-import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
-import { Stack, TextField, Typography } from "@mui/material";
+import Shop from "@/components/Shop/Shop";
 export default function Home() {
   return (
     <>
       <Header />
-      <Stack direction="row" className=" justify-between px-[378px]">
-        <Stack>
-          <Typography fontSize="22px" fontWeight="800" color="#151875">
-            Ecommerce Acceories & Fashion item{" "}
-          </Typography>
-          <Typography fontSize="12px" fontWeight="400" color="#8A8FB9">
-            About 9,620 results (0.62 seconds)
-          </Typography>
-        </Stack>
-        <Stack direction="row" className="text-[#3F509E] items-center">
-          <Stack direction="row" className="gap-[10px]">
-            <Typography fontSize="16px" fontWeight="400">
-              Per Page:
-            </Typography>
-            <TextField
-              InputProps={{
-                sx: {
-                  bgcolor: "#E7E6EF",
-                  width: "56px",
-                  height: "25px",
-                  p: "0px",
-                },
-              }}
-            />
-          </Stack>
-          <Stack direction="row">
-            <Typography>Sort By:</Typography>
-            <TextField
-              InputProps={{
-                sx: {
-                  bgcolor: "#E7E6EF",
-                  width: "98px",
-                  height: "25px",
-                  p: "0px",
-                },
-              }}
-            />
-          </Stack>
-          <Stack direction="row" className="items-center gap-1">
-            <Typography>View:</Typography>
-            <WindowIcon sx={{ width: "12px", height: "12px" }} />
-            <FormatListBulletedIcon sx={{ width: "12px", height: "12px" }} />
-            <TextField
-              InputProps={{
-                sx: {
-                  marginX: "10px",
-                  bgcolor: "#E7E6EF",
-                  width: "166px",
-                  height: "25px",
-                  p: "0px",
-                },
-              }}
-            />
-          </Stack>
-        </Stack>
-      </Stack>
       <Footer />
     </>
   );
