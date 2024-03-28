b4891b658b82fdc2e2af72d7aabdb1c9e9c8e875
Бат-Эрдэнэ Хадбаатар <23LP6564@G302-CC0111.local>
2024-03-28 17:48:51 +0800

    gg

--- frontend/src/app/productDetail/page.tsx
+++ frontend/src/app/productDetail/page.tsx
@@ -1,11 +1,72 @@
 "use client";
 import { Stack } from "@mui/material";
-import Details from "./[id]/page";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Client } from "@/components/providers/ClientProvider";
+import { api } from "@/common";
 
-export default function ProductDetail() {
+type userType = {
+  userName: string;
+  email: string;
+  merchName: string;
+  address: { city: string; district: string; khoroo: string };
+  experience: string;
+  merchType: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+type CommentType = {
+  userId: userType;
+  productId: string;
+  comment: string;
+  star: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+
+const detailId = localStorage.getItem("detailId");
+export default function ProductDetail(props: any) {
+  const { allProducts, id, refresh } = Client();
+  const [allComments, setAllComments] = useState<CommentType[]>([]);
+  const router = useRouter();
+  const selectedProduct = allProducts.find(
+    (product) => product._id == id
+  );
+  console.log(selectedProduct)
+  // const getAllComments = async () => {
+  //   try {
+  //     const { data } = await api.get("comment/getAllComments");
+  //     setAllComments(data);
+  //   } catch (error) {
+  //     console.log(error), "FFF";
+  //   }
+  // };
+  // const selectedProductComments = allComments.filter(
+  //   (comment) => comment.productId == detailId
+  // );
+
+  // useEffect(() => {
+  //   getAllComments();
+  // }, [refresh]);
+  if (!detailId) {
+    router.push("/");
+  }
   return (
     <Stack>
-      <Details />
+      <ProductDetail
+        id={selectedProduct?._id}
+        productImage={selectedProduct?.images}
+        productName={selectedProduct?.productName}
+        price={selectedProduct?.price}
+        // productAdditional={selectedProduct?.productAdditional}
+        // productColor={selectedProduct?.productColor}
+        // productRating={selectedProduct?.avgStars}
+        // reviewCount={selectedProduct?.reviewCount}
+        // comments={selectedProductComments}
+      />
     </Stack>
   );
 }
